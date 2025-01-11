68d7c5982e896e9d9209123ab1de54a9eab7db4c
HKS 27D <51793958+hks27d@users.noreply.github.com>
2025-01-11 01:45:05 +0000

    Initial release

---
+++ CSSharpServerStatus.cs
@@ -0,0 +1,97 @@
+﻿using CounterStrikeSharp.API;
+using CounterStrikeSharp.API.Core;
+using CounterStrikeSharp.API.Modules.Admin;
+using CounterStrikeSharp.API.Core.Attributes;
+using CounterStrikeSharp.API.Core.Attributes.Registration;
+using CounterStrikeSharp.API.Modules.Commands;
+using CounterStrikeSharp.API.Modules.Utils;
+using CounterStrikeSharp.API.Modules.Entities;
+
+namespace CSSharpServerStatus
+{
+    public class CSSharpServerStatus : BasePlugin
+    {
+        public override string ModuleName => "CS# Server status";
+        public override string ModuleVersion => "1.0.0";
+        public override string ModuleAuthor => "HKS 27D";
+        public override string ModuleDescription => "";
+
+        [ConsoleCommand("css_status", "Server status")]
+        [RequiresPermissions("@css/chat")]
+        [CommandHelper(whoCanExecute: CommandUsage.CLIENT_AND_SERVER)]
+        public void OnLastCommand(CCSPlayerController? Player, CommandInfo commandInfo)
+        {
+            if (Player != null && Player.IsValid && Player.PlayerPawn.IsValid && Player.Connected == PlayerConnectedState.PlayerConnected)
+            {
+                Player.PrintToChat($" {ChatColors.Red}Modders {ChatColors.Default}- See console for {ChatColors.Green}server status {ChatColors.Default}output.");
+                Player.PrintToConsole("UserID | SteamID64         | IPv4 address    | Nickname");
+                Player.PrintToConsole("-------------------------------------------------------");
+
+                int OnlinePlayers = 0;
+                var PlayersEntities = Utilities.FindAllEntitiesByDesignerName<CCSPlayerController>("cs_player_controller");
+                foreach (var PlayerEntity in PlayersEntities)
+                {
+                    if (PlayerEntity != null && PlayerEntity.IsValid && PlayerEntity.PlayerPawn.IsValid && PlayerEntity.Connected == PlayerConnectedState.PlayerConnected && PlayerEntity.UserId.HasValue)
+                    {
+                        OnlinePlayers++;
+                        if ((AdminManager.PlayerHasPermissions(PlayerEntity, "@css/root")))
+                            Player.PrintToConsole($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {"ROOT PROTECTED",-15} | {PlayerEntity.PlayerName}");
+                        else if (AdminManager.PlayerHasPermissions(PlayerEntity, "@css/generic"))
+                            Player.PrintToConsole($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {"PROTECTED",-15} | {PlayerEntity.PlayerName}");
+                        else
+                        {
+                            if (PlayerEntity.IpAddress!.Contains(':'))
+                                Player.PrintToConsole($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {PlayerEntity.IpAddress[..PlayerEntity.IpAddress.IndexOf(':')],-15} | {PlayerEntity.PlayerName}");
+                            else
+                            {
+                                Player.PrintToConsole($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {PlayerEntity.IpAddress ?? "",-15} | {PlayerEntity.PlayerName}");
+                            }
+                        }
+                    }
+                }
+                if (OnlinePlayers == 0)
+                    Player.PrintToConsole($"0 player(s) online");
+                else
+                {
+                    Player.PrintToConsole("-------------------");
+                    Player.PrintToConsole($"{OnlinePlayers,-2} player(s) online");
+                }
+            }
+            else
+            {
+                commandInfo.ReplyToCommand("UserID | SteamID64         | IPv4 address    | Nickname");
+                commandInfo.ReplyToCommand("-------------------------------------------------------");
+
+                int OnlinePlayers = 0;
+                var PlayersEntities = Utilities.FindAllEntitiesByDesignerName<CCSPlayerController>("cs_player_controller");
+                foreach (var PlayerEntity in PlayersEntities)
+                {
+                    if (PlayerEntity != null && PlayerEntity.IsValid && PlayerEntity.PlayerPawn.IsValid && PlayerEntity.Connected == PlayerConnectedState.PlayerConnected && PlayerEntity.UserId.HasValue)
+                    {
+                        OnlinePlayers++;
+                        if (AdminManager.PlayerHasPermissions(PlayerEntity, "@css/root"))
+                            commandInfo.ReplyToCommand($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {"ROOT PROTECTED",-15} | {PlayerEntity.PlayerName}");
+                        else if (AdminManager.PlayerHasPermissions(PlayerEntity, "@css/generic"))
+                            commandInfo.ReplyToCommand($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {"PROTECTED",-15} | {PlayerEntity.PlayerName}");
+                        else
+                        {
+                            if (PlayerEntity.IpAddress!.Contains(':'))
+                                commandInfo.ReplyToCommand($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {PlayerEntity.IpAddress[..PlayerEntity.IpAddress.IndexOf(':')],-15} | {PlayerEntity.PlayerName}");
+                            else
+                            {
+                                commandInfo.ReplyToCommand($"#{PlayerEntity.UserId,-5} | {PlayerEntity.SteamID,-17} | {PlayerEntity.IpAddress ?? "",-15} | {PlayerEntity.PlayerName}");
+                            }
+                        }
+                    }
+                }
+                if (OnlinePlayers == 0)
+                    commandInfo.ReplyToCommand($"0 player(s) online");
+                else
+                {
+                    commandInfo.ReplyToCommand("-------------------");
+                    commandInfo.ReplyToCommand($"{OnlinePlayers,-2} player(s) online");
+                }
+            }
+        }
+    }
+};
---
+++ README.md
@@ -0,0 +1,28 @@
+[![Discord](https://img.shields.io/discord/1323042994437357599?style=for-the-badge)](https://discord.com/invite/zsmUzthPXx)
+
+# 📌 CS# Server Status (console support)
+Get the server status (#userid, SteamID64, IPv4 address, nickname and sum of all online players).
+
+## 🌐 Description
+Shows a list of all the players connected to the server. It also has console support. The IPv4 address for root is "ROOT PROTECTED" and for generic admins is "PROTECTED".
+
+## 📗 Dependecies
+- [CounterStrikeSharp](https://github.com/roflmuffin/CounterStrikeSharp)
+
+## 📁 Installation
+1. Get an IDE & Code Editor (Visual Studio, Rider or others).
+2. Install package 'Microsoft.Extensions.Logging.Abstractions'.
+3. Edit and debug if you need to.
+4. Build the code.
+5. Add the files (`.dll`, `.pdb`, `.deps.json`) in their own directory.
+6. Rename directory. Use the same name.
+7. Upload directory in `/game/csgo/addons/counterstrikesharp/plugins`.
+
+## 🤝 Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## ⭐ Show your support
+Give a ⭐ if this project helped you.
+
+## 📝 License
+MIT based on outlined exemption from CounterStrikeSharp.
